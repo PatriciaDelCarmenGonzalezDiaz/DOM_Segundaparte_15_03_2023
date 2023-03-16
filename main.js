c320fa2e0fa25f5e0a74b55ebcc1d7b6f9ed8872
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-03-15 21:07:50 -0500

    Propagacion_de_eventos_1

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
+let boton = document.querySelector("button");
 let root = document.querySelector(".root");
-let boton = document.querySelector(".btn");
 
-boton.addEventListener("click", () => {
-    boton.dispatchEvent(new CustomEvent("user:message", {
-      detail: {
-        name: "Manz"
-      },
-      bubbles: true,
-    }));
-  });
-  
-  root.addEventListener("user:message", (event) => {
-    let name = event.detail.name;
-    let number = event.target.dataset.number;
-    console.log(`Mensaje recibido de ${name} (${number})`);
-  }, { capture: true });
+boton.addEventListener("click", (event) => {
+  let customEvent = new CustomEvent("warning", { bubbles: true });
+  boton.dispatchEvent(customEvent);
+});
+
+root.addEventListener("warning", (event) => {
+  console.log("Evento click recibido en el root.", event);
+});
