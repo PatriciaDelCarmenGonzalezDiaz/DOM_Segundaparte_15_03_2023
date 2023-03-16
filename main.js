95f0306b3900f4566a5238b2b74ef88ca72bbbf6
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-03-15 20:58:52 -0500

    Propagacion_de_eventos

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-let texto = document.querySelector(".text");
+let root = document.querySelector(".root");
+let boton = document.querySelector(".btn");
 
-document.body.addEventListener("click", (event)=> {
-    let {x,y,detail} = event;
-    texto.textContent = `Has hecho ${detail} CLICK en las coordenadas (${x} y ${y})`;
-});
+boton.addEventListener("click", () => {
+    boton.dispatchEvent(new CustomEvent("user:message", {
+      detail: {
+        name: "Manz"
+      },
+      bubbles: true,
+    }));
+  });
+  
+  root.addEventListener("user:message", (event) => {
+    const name = event.detail.name;
+    const number = event.target.dataset.number;
+    console.log(`Mensaje recibido de ${name} (${number})`);
+  });
