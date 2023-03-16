71524c36f5963a5df3fd4cf4f3010b5b0b6763f8
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-03-15 20:20:37 -0500

    Evento_Boton_Escuchar_Multiples_eventos

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let boton = document.querySelector("#btn");
 let eventManager = {
     handleEvent: function(event) {
-        alert("Hola, Has presionado un Botón");
+        if (event.type == "click") {
+            alert("Hola, Has presionado un Botón");
+        } else if (event.type == "mouseleave") {
+            alert("Adios, has abandonado el botón");
+        }
     }
 }
 
 boton.addEventListener("click",eventManager);
+boton.addEventListener("mouseleave",eventManager);
