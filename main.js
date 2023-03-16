3465a51efeaa708683725c521d6a8ff6f3669223
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-03-15 19:57:52 -0500

    Evento_Boton_Multiples_Listeners

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let boton = document.querySelector("#btn");
 let accion = () => alert("Hola, Presionaste un botón");
+let alternar = () => boton.classList.toggle("red");
 
 boton.addEventListener("click", accion);
+boton.addEventListener("click", alternar);
