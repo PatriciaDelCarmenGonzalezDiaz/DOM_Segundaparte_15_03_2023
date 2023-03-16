b0185a335ac21094ec5eb10f2a97082fb1a778d6
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-03-15 20:29:41 -0500

    Evento_Boton_Objeto_Event

--- main.js
+++ main.js
@@ -1,24 +1,5 @@
 let boton = document.querySelector("#btn");
-class eventManager {
-    handleEvent(event) {
-        if (event.type == "click") {
-            this.onClick(event.type, event.target);
-        } else if (event.type == "mouseleave") {
-            this.onLeave(event.type, event.target);
-        }
-    }
-    
-    onClick(type, element) {
-        alert("Hola, Has presionado un Botón");
-        console.log({type, element});
-    }
 
-    onLeave(type, element) {
-        alert("Adios, has abandonado el botón");
-        console.log({type, element});
-    }
-}
-
-let ev = new eventManager();
-boton.addEventListener("click",ev);
-boton.addEventListener("mouseleave",ev);
+boton.addEventListener("click", (event)=>{
+    console.log(event);
+})
